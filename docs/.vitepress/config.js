510a720d25a198836ad3f64a28202316271c67fb
willyon <willyonzhon@gmail.com>
2024-07-31 21:17:35 +0800

    commit third time

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,9 +2,23 @@
  * @Author: zhangshouchang
  * @Date: 2024-07-31 19:34:14
  * @LastEditors: zhangshouchang
- * @LastEditTime: 2024-07-31 19:34:15
+ * @LastEditTime: 2024-07-31 21:14:44
  */
 module.exports = {
-  title: 'My Blog',
-  description: 'A VitePress site',
+  base: '/technology-blog/',
+  title: '我的博客',
+  description: '使用 VuePress 创建的博客',
+  themeConfig: {
+    navbar: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' }
+    ],
+    sidebar: [
+      {
+        title: '指南',
+        children: ['/guide/']
+      }
+    ]
+  }
 }
+
