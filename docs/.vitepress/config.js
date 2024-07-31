9d26f3f14e48508c5c223600e3e55d16c445eeab
willyon <willyonzhon@gmail.com>
2024-07-31 19:38:42 +0800

    commit first time

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: zhangshouchang
+ * @Date: 2024-07-31 19:34:14
+ * @LastEditors: zhangshouchang
+ * @LastEditTime: 2024-07-31 19:34:15
+ */
+module.exports = {
+  title: 'My Blog',
+  description: 'A VitePress site',
+}
