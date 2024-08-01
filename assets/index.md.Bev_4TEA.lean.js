ad4ae3af3fea2dce11f497afc7a556d1430cef57
willyon <willyon@users.noreply.github.com>
2024-08-01 06:48:29 +0000

    deploy: 8fe6a44566a9687681dde6081cecfaa3595ff16b

---
+++ assets/index.md.Bev_4TEA.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a2 as o}from"./chunks/framework.B7XykaSp.js";const m=JSON.parse('{"title":"Hello VitePress","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),s={name:"index.md"},i=o("",5),r=[i];function l(n,h,d,c,_,p){return t(),a("div",null,r)}const b=e(s,[["render",l]]);export{m as __pageData,b as default};
